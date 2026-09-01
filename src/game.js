8352fa079afd065b697024a74808c6607f82ee43
sascov5-eng <sascov5@gmail.com>
2026-09-01 21:40:15 +0300

    Force visible bootstrap test scene

--- src/game.js
+++ src/game.js
@@ -1,4 +1,24 @@
-import { ApproachScene } from './scenes/ApproachScene.js';
+const TestScene = new Phaser.Class({
+  Extends: Phaser.Scene,
+  initialize() { Phaser.Scene.call(this, { key: 'TestScene' }); },
+  create() {
+    this.add.text(40, 40, 'Ashen Hollow Android TEST', { fontSize: '32px', color: '#ffffff' });
+    this.add.rectangle(640, 650, 1000, 40, 0x3b82f6);
+    this.player = this.add.rectangle(200, 550, 50, 80, 0xffffff);
+    this.enemy = this.add.rectangle(800, 570, 60, 60, 0xff0000);
+    this.keys = this.input.keyboard.createCursorKeys();
+    this.add.text(40, 100, 'LEFT RIGHT / UP', { fontSize: '24px', color: '#aaaaaa' });
+    this.cameras.main.startFollow(this.player);
+    this.physics.add.existing(this.player);
+    this.player.body.setCollideWorldBounds(true);
+    this.physics.add.existing(this.enemy);
+  },
+  update() {
+    if (this.keys.left.isDown) this.player.x -= 5;
+    if (this.keys.right.isDown) this.player.x += 5;
+    if (this.keys.up.isDown) this.player.y -= 5;
+  }
+});
 
 const config = {
   type: Phaser.AUTO,
@@ -6,18 +26,8 @@ const config = {
   width: 1280,
   height: 720,
   backgroundColor: '#070a10',
-  physics: {
-    default: 'arcade',
-    arcade: { gravity: { y: 0 }, debug: false },
-  },
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-  },
-  input: {
-    activePointers: 8,
-  },
-  scene: [ApproachScene],
+  scene: [TestScene],
+  scale: { mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH }
 };
 
 new Phaser.Game(config);
