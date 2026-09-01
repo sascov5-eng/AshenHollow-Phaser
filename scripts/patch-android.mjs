8d313117dfd7bb4d19b23a0306bd81bc7317e1bd
sascov5-eng <sascov5@gmail.com>
2026-09-01 19:37:43 +0300

    Force landscape orientation in Android activity

--- scripts/patch-android.mjs
+++ scripts/patch-android.mjs
@@ -1,11 +1,37 @@
 import { readFile, writeFile } from 'node:fs/promises';
 
-const path = 'android/app/src/main/AndroidManifest.xml';
-let manifest = await readFile(path, 'utf8');
-if (!manifest.includes('android:screenOrientation="landscape"')) {
-  manifest = manifest.replace(
-    '<activity',
-    '<activity android:screenOrientation="landscape"',
-  );
+const manifestPath = 'android/app/src/main/AndroidManifest.xml';
+let manifest = await readFile(manifestPath, 'utf8');
+manifest = manifest.replace(/android:screenOrientation="[^"]*"/g, '');
+manifest = manifest.replace(
+  '<activity',
+  '<activity android:screenOrientation="sensorLandscape"',
+);
+await writeFile(manifestPath, manifest, 'utf8');
+
+const mainActivityPath = 'android/app/src/main/java/com/ashenhollow/android/MainActivity.java';
+const mainActivity = `package com.ashenhollow.android;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+            View.SYSTEM_UI_FLAG_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+    }
 }
-await writeFile(path, manifest, 'utf8');
+`;
+await writeFile(mainActivityPath, mainActivity, 'utf8');
